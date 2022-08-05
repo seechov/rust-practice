cfaf61ed2e910e27f978d4cb8589d5bd689fadb6
Алексей Сычёв <seechov@gmail.com>
2022-08-05 17:43:46 +0300

    Implement match usage.

--- 02_guessing_game/src/main.rs
+++ 02_guessing_game/src/main.rs
@@ -6,21 +6,30 @@ fn main() {
     println!("Угадайте число!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("Секретное чило равно {}", secret_number);
-    println!("Пожалуйста, введите свою догадку.");
-    let mut guess = String::new();
+    // println!("Секретное чило равно {}", secret_number);
 
-    io::stdin()
+    loop {
+        println!("Пожалуйста, введите свою догадку.");
+        let mut guess = String::new();
+
+        io::stdin()
         .read_line(&mut guess)
         .expect("Не получилось прочитать строку");
 
-    let guess: u32 = guess.trim().parse().expect("Пожалуйста, наберите число!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Слишком малое число!"),
-        Ordering::Greater => println!("Слишком большое число!"),
-        Ordering::Equal => println!("Вы выйграли!"),
+        println!("Вы загадали: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Слишком малое число!"),
+            Ordering::Greater => println!("Слишком большое число!"),
+            Ordering::Equal => {
+                println!("Вы выйграли!");
+                break;
+            },
+        }
     }
-
-    println!("Вы загадали: {}", guess);
 }
